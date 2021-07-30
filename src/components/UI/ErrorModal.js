d3f42f0da420ab49ecd64c9d179b95e3aede5b22
jeromequah <jerome.quah.2018@sis.smu.edu.sg>
2021-07-30 14:53:13 +0800

    ReactDOM Portals for Backdrop, Overlay from ErrorModal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,30 +1,48 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import ReactDOM from 'react-dom';
+
 import Card from "./Card";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css"
-import Wrapper from "../Helpers/Wrapper";
+
+function Backdrop(props) {
+    return <div className={styles.backdrop} onClick={props.onConfirm}/>
+}
+
+function ModalOverlay(props) {
+    return (
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>
+                    {props.title}
+                </h2>
+            </header>
+            <div className={styles.content}>
+                <p>
+                    {props.message}
+                </p>
+            </div>
+            <footer className={styles.actions}>
+                <Button onClick={props.onConfirm}>Okay!</Button>
+            </footer>
+        </Card>
+    )
+}
 
 function ErrorModal(props) {
     return (
-        <Wrapper>
-            <div className={styles.backdrop} onClick={props.onConfirm}>
-            </div>
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>
-                        {props.title}
-                    </h2>
-                </header>
-                <div className={styles.content}>
-                    <p>
-                        {props.message}
-                    </p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button onClick={props.onConfirm}>Okay!</Button>
-                </footer>
-            </Card>
-        </Wrapper>
+        <Fragment>
+            {ReactDOM.createPortal
+            (
+                <Backdrop onConfirm={props.onConfirm}/>,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal
+            (
+                <ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>,
+                document.getElementById('overlay-root')
+            )}
+        </Fragment>
     )
 }
 
